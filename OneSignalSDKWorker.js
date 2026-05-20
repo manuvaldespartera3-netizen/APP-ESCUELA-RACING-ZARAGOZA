c36df6379707a7b186788f2c6e75abc93384c1fe
manuvaldespartera3-netizen <manuvaldespartera3@gmail.com>
2026-05-20 12:10:43 +0200

    Add files via upload

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,9 +1,1 @@
-<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
-<script>
-  window.OneSignalDeferred = window.OneSignalDeferred || [];
-  OneSignalDeferred.push(async function(OneSignal) {
-    await OneSignal.init({
-      appId: "17f29279-cbee-4f62-9e91-e470c318844c",
-    });
-  });
-</script>
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
