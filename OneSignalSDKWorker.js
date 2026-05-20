511d245a4997a68ee0a89408c3a49b5777e70224
manuvaldespartera3-netizen <manuvaldespartera3@gmail.com>
2026-05-20 10:04:25 +0200

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,9 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+<script>
+  window.OneSignalDeferred = window.OneSignalDeferred || [];
+  OneSignalDeferred.push(async function(OneSignal) {
+    await OneSignal.init({
+      appId: "17f29279-cbee-4f62-9e91-e470c318844c",
+    });
+  });
+</script>
